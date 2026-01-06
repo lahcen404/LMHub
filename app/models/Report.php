c5ab5b087dbc6ff9d98e4cbfc859481dd7f0a7d3
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-06 09:44:05 +0100

    create Model Report

--- app/models/Report.php
+++ app/models/Report.php
@@ -10,21 +10,33 @@ class Report extends Model{
     private ?int $id;
     private string $reason;
     private Status $status;
+    private int $readerId;
+    private int $articleId;
+    private int $commentId; 
 
-    public function __construct(?int $id = null , string $reason = '', Status $status = Status::PENDING)
+    public function __construct(?int $id = null , string $reason = '', Status $status = Status::PENDING, int $readerId = 0, int $articleId = 0, int $commentId = 0)
     {
          parent::__construct();
          $this->id = $id;
          $this->reason = $reason;
          $this->status = $status;
+         $this->readerId = $readerId;
+         $this->articleId = $articleId;
+         $this->commentId = $commentId;
     }
 
     // getters
     public function getId(): ?int { return $this->id; }
     public function getReason(): string { return $this->reason; }
     public function getStatus(): Status { return $this->status; }
+    public function getReaderId(): int { return $this->readerId; }
+    public function getArticleId(): int { return $this->articleId; }
+    public function getCommentId(): int { return $this->commentId; }
 
     // setters
     public function setReason(string $reason): void { $this->reason = $reason; }
     public function setStatus(Status $status): void { $this->status = $status; }
+    public function setReaderId(int $readerId): void { $this->readerId = $readerId; }
+    public function setArticleId(int $articleId): void { $this->articleId = $articleId; } 
+    public function setCommentId(int $commentId): void { $this->commentId = $commentId; }  
 }
