c156a321250628768de5605e0311de241b73d940
lahcen404 <lahcen.maskour2003@gmail.com>
2026-01-09 10:28:12 +0100

    create function of display profile infos

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,15 +3,33 @@
 namespace app\controllers;
 
 use app\models\Reader;
+use app\helpers\debug;
 use app\core\Controller;
 use PDO;
+use app\config\DBConnection;
 
 
 class UserController extends Controller{
 
+    private $db;
+
+    public function __construct(){
+        $this->db = DBConnection::getInstance()->connectDB();
+    }
+
+
+
      public function profile()
     {
-        $this->view('profile', ['title' => 'User Profile | LMHub']);
+        if (session_status() === PHP_SESSION_NONE) session_start();
+
+        $id = $_SESSION['user_id'];
+
+        
+
+        $user = self::getById($this->db,$id);
+        $this->view('profile', ['title' => 'User Profile | LMHub',
+                                'user'=> $user]);
     }
 
      public static function save($pdo, $name, $email, $password, $role = 'READER')
@@ -25,6 +43,17 @@ class UserController extends Controller{
             'pass'  => $password,
             'role'  => $role
         ]);
+    }
+
+    public static function getById($pdo ,$id){
+
+        $sql = "SELECT * FROM users WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+
+        $user = $stmt->fetch();
+        return $user ?: null;
+
     }
 
       public static function findByEmail($pdo, $email)
